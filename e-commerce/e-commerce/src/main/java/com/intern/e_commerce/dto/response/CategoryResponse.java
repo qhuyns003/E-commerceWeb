5fac8be052a520dbfefa768a3f64de33629cd52c
tuanhy1702 <tuankutehy@gmail.com>
2025-03-05 12:40:32 +0700

    PS C:\Users\ADMIN\OneDrive\Desktop\E-commerceWeb> git checkout main PS C:\Users\ADMIN\OneDrive\Desktop\E-commerceWeb> git checkout main error: The following untracked working tree files would be overwritten by checkout:         e-commerce/e-commerce/src/main/java/com/intern/e_commerce/controller/CategoryController.java         e-commerce/e-commerce/src/main/java/com/intern/e_commerce/dto/request/CategoryCreateRequest.java         e-commerce/e-commerce/src/main/java/com/intern/e_commerce/dto/response/CategoryResponse.java         e-commerce/e-commerce/src/main/java/com/intern/e_commerce/mapper/CategoryMapper.java         e-commerce/e-commerce/src/main/java/com/intern/e_commerce/repository/CategoryRepository.java         e-commerce/e-commerce/src/main/java/com/intern/e_commerce/service/CategoryService.java Please move or remove them before you switch branches. Aborting error: The following untracked working tree files would be overwritten by checkout:         e-commerce/e-commerce/src/main/java/com/intern/e_commerce/controller/CategoryController.java         e-commerce/e-commerce/src/main/java/com/intern/e_commerce/dto/request/CategoryCreateRequest.java         e-commerce/e-commerce/src/main/java/com/intern/e_commerce/dto/response/CategoryResponse.java         e-commerce/e-commerce/src/main/java/com/intern/e_commerce/mapper/CategoryMapper.java         e-commerce/e-commerce/src/main/java/com/intern/e_commerce/repository/CategoryRepository.java         e-commerce/e-commerce/src/main/java/com/intern/e_commerce/service/CategoryService.java Please move or remove them before you switch branches. Aborting

---
+++ e-commerce/e-commerce/src/main/java/com/intern/e_commerce/dto/response/CategoryResponse.java
@@ -0,0 +1,16 @@
+package com.intern.e_commerce.dto.response;
+
+import lombok.*;
+import lombok.experimental.FieldDefaults;
+
+import java.time.LocalDate;
+import java.util.Set;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@FieldDefaults(level = AccessLevel.PRIVATE)
+public class CategoryResponse {
+    String name;
+}
